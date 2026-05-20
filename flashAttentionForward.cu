51f4b60cd7d8451c565fc5628dd523a1ad8f8b62
Raptor-1772791874 <1772791874@qq.com>
2026-05-21 03:13:46 +0800

    Update flashAttentionForward.cu
    重新修改了QKVO的类型以及FragS的声明位置，以及重新更新了搬运去SRAM时的索引公式，重新给搬运FragK打上补丁（之前没写搬运K），给多个语法细节修复

--- flashAttentionForward.cu
+++ flashAttentionForward.cu
@@ -1,11 +1,9 @@
 #include <cuda_runtime.h>
-#include <mma.h>
 #include <cuda_fp16.h>
+#include <mma.h>
 #include <iostream>
 using  namespace nvcuda::wmma;
 
-
-wmma::fragment<wmma::accumulator,16,16,16,float>frag_S[4][4];
 
 
 #define Block_Size 128
@@ -13,9 +11,9 @@ wmma::fragment<wmma::accumulator,16,16,16,float>frag_S[4][4];
 
 
 void launch_flash_atten_forward(
-    float* Q,float* K,float* V,float* O,
+    half* Q,half* K,half* V,half* O,
     int* cu_seqlens,
-    int* cu_seqlen_blocks
+    int* cu_seqlen_blocks,
     int total_tokens,
     int total_blocks
     int num_heads,
@@ -45,12 +43,12 @@ void launch_flash_atten_forward(
    //restrict是告诉编译器指针不重叠继续压榨性能
 
     __global__ void flash_atten_kernel(
-        const float* __restrict__ Q,
-        const float* __restrict__ K,
-        const float* __restrict__ V,
-        float* __restrict__ O,
+        const half* __restrict__ Q,
+        const half* __restrict__ K,
+        const half* __restrict__ V,
+        half* __restrict__ O,
         const int* __restrict__ cu_seqlens,
-        const int* __restrict__ cu_seqlens_blocks;
+        const int* __restrict__ cu_seqlens_blocks,
         int total_tokens,
         int num_heads){
 
@@ -83,12 +81,12 @@ void launch_flash_atten_forward(
 
             //用第几个block乘以大小后这里才是线程负责的token所在的全规格矩阵里第一个token全局起点（此背景是全局偏移且是4096开头）
             //接上，意思是它没有走到自己负责的头的真实地址上还要写偏移公式。
-            int global_q_start=seq_start+(relative_chunk_idx*Block_Size);
+            int global_q_start=seq_start+(relative_chunk_idx*Block_Size);//在Q的全局偏移背景下
             
             //先划定一个sharedMemory大小是多大，128个token128个维度
-            __shared__ __half s_Q[Block_Size][Head_Dim];
-            __shared__ __half s_K[Block_Size][Head_Dim];
-            __shared__ __half s_V[Block_Size][Head_Dim];
+            __shared__ half s_Q[Block_Size][Head_Dim];
+            __shared__ half s_K[Block_Size][Head_Dim];
+            __shared__ half s_V[Block_Size][Head_Dim];
             
 
             //并未在sharedmemory里，这是每个线程自己寄存器里的
@@ -98,10 +96,11 @@ void launch_flash_atten_forward(
             //用来装最终的累加器O_local，这个线程负责当前词的128个维度所以开了一个一维数组存
             float o_local[Head_Dim]={0.0f};
 
-            const int f4_stride=Head_Dim/4;
+            //一个float4任务可以搬16个字节即8个half数据所以除8
+            const int f4_stride=Head_Dim/8;
 
             //直接降落到当前线程负责的token的block里第0个token的开头，具体见笔记
-            int Not_sure_index=global_q_start *num_heads+head_idx;//（此背景也是全局偏移）
+            int Not_sure_index=global_q_start *num_heads+head_idx;//（此背景是Q的全局偏移而非显存全局偏移）
 
             const float4* Q_f4 = reinterpret_cast<const float4*>(Q + Not_sure_index * Head_Dim);
             float4* s_Q_f4 = reinterpret_cast<float4*>(s_Q);
@@ -114,15 +113,15 @@ void launch_flash_atten_forward(
             //准备搬运Q进s_Q。再for搬运KV，然后点积
 
             #pragma unroll
-            for(int i=0;i<32;++i){
-                //128个token有32个float4要搬一共4096次，一次搬运了128个float4，tid是每个线程在当前搬运里的相对偏移
+            for(int i=0;i<16;++i){
+                //1个token有16个float4要搬一共128token则有2048个任务，一次搬运了128个float4所以16次循环就搬完了，tid是每个线程在当前搬运里的相对偏移
                 int index=i*128+tid;//每个线程单独再编号，一次i循环会搬完128个float4，用这个保持偏移的精准
                 //index表示当前线程负责的具体哪个float4任务
 
-                int token_offset=index/32;
-                int Dim_offset=index%32;
+                int token_offset=index/16;
+                int Dim_offset=index%16;
                 // 计算当前正在搬运的Token 的全局索引
-                int current_global_token = global_q_start + token_offset;//此背景也是全局偏移下的
+                int current_global_token = global_q_start + token_offset;//此背景也是全局token数偏移下的
                 
                 // 越界防线：绝不跨入下一个用户的领地，也不超出当前 seq_end！
                 if(current_global_token < seq_end) {
@@ -153,10 +152,10 @@ void launch_flash_atten_forward(
 
                 //重复一下在搬运Q已经实践过的逻辑，把KV也搬走
                 #pragma  unroll
-                for(int i=0;i<32;++i){
-                    int  index =i*128+tid;//算自己搬运4096个floa4任务里的哪一个（背景是4096偏移），一个token有32个float4任务
-                    int token_offset=index/32;//算自己负责哪个token
-                    int Dim_offset=index%32;//算自己负责token里的哪个float4任务
+                for(int i=0;i<16;++i){
+                    int  index =i*128+tid;//算自己搬运2048个floa4任务里的哪一个（背景是4096偏移），一个token有16个float4任务
+                    int token_offset=index/16;//算自己负责哪个token
+                    int Dim_offset=index%16;//算自己负责token里的哪个float4任务
 
 
                     //查询当前token在数值上是否合法（在用户范围内）
@@ -167,7 +166,7 @@ void launch_flash_atten_forward(
                         //计算公式见补充5，与Q一致不再此过多赘述
                         int global_read_idx=token_offset*(num_heads*f4_stride)+Dim_offset;
                         s_K_f4[index]=K_f4[global_read_idx];
-                        s_V_f4[index]=Q_f4[global_read_idx];
+                        s_V_f4[index]=V_f4[global_read_idx];
 
                     }//如果越界还是填0，不允许不填（破坏了矩阵完整性宁愿计算无效值也要保证矩阵乘法时满足法则）
                     else{
@@ -183,6 +182,9 @@ void launch_flash_atten_forward(
 
                 //算S矩阵了这下......
 
+                //先声明FragS在线程寄存器里
+                wmma::fragment<wmma::accumulator,16,16,16,float>frag_S[4][4];
+
                 //将存放结果的结构体数组都先初始化为0
                 #pragma unroll
                 for(int i=0;i<4;++i){
@@ -196,17 +198,26 @@ void launch_flash_atten_forward(
                 为什么QK各要4个还是因为tensor core，每个大小为16*16（64/16为4）
                 类型为half半精度还是为了迎合tensor core*/
 
+
                 wmma::fragment<wmma::matrix_a,16,16,16,half,wmma::row_major> frag_Q[4];
                 wmma::fragment<wmma::matrix_b,16,16,16,half,wmma::col_major> frag_K[4];
 
                 /*时间线推进放在最外面，是因为fragS层面其他格子可以有数据的复用虽然单个fragS格子用不着
-                为啥是8次因为一次只能算16个token的16个维度*/
+                为啥是8次因为一次只能算16个token的16个维度*具体见补_充_2*/
                 #pragma unroll
                 for(int t_step=0;t_step<8;++t_step){
 
+                   //固定好SRAM大小，为128
+                    const int Stride_Sram=128;
+
                     #pragma unroll
                     for(int i=0;i<4;++i){
-                        wmma::load_matrix_sync(frag_Q[i],s_Q+i*16*stride_Q+t_step*16,stride_Q);
+                        wmma::load_matrix_sync(frag_Q[i],s_Q+i*16*Stride_Sram+t_step*16,Stride_Sram);
+                    }
+
+                    #pragma unroll
+                    for(int j=0;j<4;++j){
+                        wmma::load_matrix_sync(frag_K[j],s_K+j*16*Stride_Sram+t_step*16,Stride_Sram);
                     }
 
                     #pragma unroll
